1b5cfd7fe04b4ee8adc20da97b0251139605ed9b
BradL <34219856+bradlindblad@users.noreply.github.com>
2023-01-27 20:21:10 -0600

    added try catch to skip bad URLs

--- for_yoto.py
+++ for_yoto.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 
 
 FILE = input(
-    "File path of text file with youtube URLS: (enter if /Desktop/in_yoto.txt)"
+    "File path of text file with youtube URLS: (ENTER if /Desktop/in_yoto.txt) "
 )
 
 if FILE == "":
@@ -22,15 +22,23 @@ def get_vid(URL):
 
 
 if __name__ == "__main__":
-    list(map(get_vid, to_get))
+    for i in to_get:
+        try:
+            get_vid(i)
+        except:
+            print(f'URL: { i } bad ...')
+            pass
+
+    # list(map(get_vid, to_get))
     os.system(
         "audioconvert convert /home/brad/Desktop/raw_downloads /home/brad/Desktop/to_yoto"
     )
+    os.system("cp -R /home/brad/Desktop/to_yoto/. /home/brad/Music/yoto_downloads")
     os.system("rm -r /home/brad/Desktop/raw_downloads")
     print(
         """
-    
-          Now... open my.yotoplay.com and upload
+          Copied files to ~/Music/yoto_downloads ...
+          Now... open my.yotoplay.com and upload ...
           
-          """
+        """
     )
